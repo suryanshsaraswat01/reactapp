56b3af8b33b15d906aeb003f454e0381133cf20c
suryanshsaraswat01 <46866579+suryanshsaraswat01@users.noreply.github.com>
2020-09-26 01:54:54 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import react from 'react';
+
+class App extends React.Component {
+        render() {
+            return ( <
+                h1 > Youtube clone app < /h1>)
+            }
+        }
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+
+import App from './app';
+
+ReactDOM.render( < App / > , document.querySelector('root'));
